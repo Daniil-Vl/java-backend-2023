ca0aae3f8d54b4583b782a28ae45f99627308b71
Daniil <vlasov.daniil03@gmail.com>
2023-10-05 14:43:03 +0300

    polishing

--- src/main/java/edu/hw1/task5/Solution5.java
+++ src/main/java/edu/hw1/task5/Solution5.java
@@ -1,15 +1,16 @@
 package edu.hw1.task5;
 
-import java.math.BigInteger;
-
 public class Solution5 {
     private Solution5() {
     }
 
+    @SuppressWarnings("MagicNumber")
     public static boolean isPalindromeDescendant(long num) {
 
         // Return false if num have only one digit
-        if (num / 10 == 0) return false;
+        if (num / 10 == 0) {
+            return false;
+        }
 
         // Compare reversed num to original one
         long reverse = 0;
@@ -24,7 +25,8 @@ public class Solution5 {
         long descendant = 0;
         String numStr = String.valueOf(num);
         for (int i = 0; i + 1 < numStr.length(); i += 2) {
-            descendant = descendant * 10 + Character.getNumericValue(numStr.charAt(i)) + Character.getNumericValue(numStr.charAt(i + 1));
+            descendant = descendant * 10 + Character.getNumericValue(numStr.charAt(i))
+                + Character.getNumericValue(numStr.charAt(i + 1));
         }
 
         return isPalindrome || isPalindromeDescendant(descendant);
