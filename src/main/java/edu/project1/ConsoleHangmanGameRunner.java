c3207b8b7c1b943249ae8d6ea87179d8f52eff8d
Daniil-Vl <vlasov.daniil03@gmail.com>
2023-10-14 16:23:24 +0300

    Project1: implement opportunity to start new game without reopening program

--- src/main/java/edu/project1/ConsoleHangmanGameRunner.java
+++ src/main/java/edu/project1/ConsoleHangmanGameRunner.java
@@ -9,44 +9,39 @@ import org.apache.logging.log4j.Logger;
 public class ConsoleHangmanGameRunner {
 
     private final static Logger LOGGER = LogManager.getLogger();
-
-    private final static HangmanGame GAME = new HangmanGame(Dictionary.getRandomWord(), 6);
     private final static Scanner SCANNER = new Scanner(System.in);
+    private final static int MAX_NUMBER_OF_MISTAKES = 6;
+    private static String line;
 
     private ConsoleHangmanGameRunner() {
     }
 
-    private static void run() {
-        String line;
+    private static void startNewGame() throws NoSuchElementException {
+        HangmanGame game = new HangmanGame(Dictionary.getRandomWord(), MAX_NUMBER_OF_MISTAKES);
 
-        while (!GAME.isGameOver() && !GAME.isPlayerWin()) {
+        while (!game.isGameOver() && !game.isPlayerWin()) {
             LOGGER.info("Guess a letter:");
 
-            try {
-                line = SCANNER.nextLine().strip();
-            } catch (NoSuchElementException exc) {
-                LOGGER.info("Hangman game termination...");
-                break;
-            }
+            line = SCANNER.nextLine().strip();
 
             if (line.length() > 1) {
                 LOGGER.warn("You must enter just one character. Please try again.");
                 continue;
             }
 
-            GAME.guessLetter(line.charAt(0));
+            game.guessLetter(line.charAt(0));
 
-            LOGGER.info(GAME.getFeedbackMessage());
+            LOGGER.info(game.getFeedbackMessage());
 
-            LOGGER.info("The word: " + GAME.getWordCurrentState());
+            LOGGER.info("The word: " + game.getWordCurrentState());
 
-            if (GAME.isPlayerWin()) {
-                LOGGER.info(GAME.getWinMessage());
+            if (game.isPlayerWin()) {
+                LOGGER.info(game.getWinMessage());
                 break;
             }
 
-            if (GAME.isGameOver()) {
-                LOGGER.info(GAME.getGameOverMessage());
+            if (game.isGameOver()) {
+                LOGGER.fatal(game.getGameOverMessage());
                 break;
             }
         }
@@ -56,7 +51,19 @@ public class ConsoleHangmanGameRunner {
 //    public static void main(String[] args) {
 //        LOGGER.info("Hangman game starts...");
 //        LOGGER.info("You can stop game by pressing Ctrl + D");
-//        run();
+//
+//        do {
+//            try {
+//                LOGGER.info("A new word was made up. Try to find it.");
+//                startNewGame();
+//                LOGGER.warn("If you want to play again enter 'yes', otherwise press enter button: ");
+//                line = SCANNER.nextLine();
+//            } catch (NoSuchElementException exc) {
+//                LOGGER.info("Hangman game termination...");
+//                break;
+//            }
+//        } while (line.strip().equalsIgnoreCase("yes"));
+//
 //        LOGGER.info("Hangman game ends...");
 //    }
 }
