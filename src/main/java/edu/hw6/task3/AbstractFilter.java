22aa2fa3620b67647e893ab6529cdd1b72f2896c
Daniil-Vl <vlasov.daniil03@gmail.com>
2023-11-25 15:40:15 +0300

    Hw6: add new solution for task 3

--- src/main/java/edu/hw6/task3/AbstractFilter.java
+++ src/main/java/edu/hw6/task3/AbstractFilter.java
@@ -3,9 +3,15 @@ package edu.hw6.task3;
 import java.nio.file.DirectoryStream;
 import java.nio.file.Path;
 
+@FunctionalInterface
 public interface AbstractFilter extends DirectoryStream.Filter<Path> {
     /**
-     * Method for chains
+     * Method for linking filters into a chain
+     *
+     * @param filter - next filter to add in chain
+     * @return AbstractFilter, which skips the file based on its own conditions and the argument filter condition
      */
-    AbstractFilter and(AbstractFilter nextFilter);
+    default AbstractFilter and(AbstractFilter filter) {
+        return path -> accept(path) && filter.accept(path);
+    }
 }
