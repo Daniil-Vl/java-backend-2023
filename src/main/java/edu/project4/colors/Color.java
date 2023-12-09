714a324c71e31ef4b789dc960c5f00d6f7d7e013
Daniil-Vl <vlasov.daniil03@gmail.com>
2023-12-09 11:25:38 +0300

    Project4: add new colors

---
+++ src/main/java/edu/project4/colors/Color.java
@@ -0,0 +1,32 @@
+package edu.project4.colors;
+
+public enum Color {
+    RED(255, 0, 0),
+    GREEN(0, 255, 0),
+    BLUE(0, 0, 255),
+    WHITE(255, 255, 255),
+    BLACK(0, 0, 0),
+    PURPLE(158, 103, 210);
+
+    private final int red;
+    private final int green;
+    private final int blue;
+
+    Color(int red, int green, int blue) {
+        this.red = red;
+        this.green = green;
+        this.blue = blue;
+    }
+
+    public int getRed() {
+        return red;
+    }
+
+    public int getGreen() {
+        return green;
+    }
+
+    public int getBlue() {
+        return blue;
+    }
+}
