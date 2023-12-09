4ab066a6a81fdc6679fc7c373a8e3421d61afa2c
Daniil <vlasov.daniil03@gmail.com>
2023-12-09 22:30:30 +0300

    Project4: add new generation method

--- src/main/java/edu/project4/FlameGenerationRunner.java
+++ src/main/java/edu/project4/FlameGenerationRunner.java
@@ -5,13 +5,17 @@ import edu.project4.image.FractalImage;
 import edu.project4.imageprocessing.LogGammaCorrection;
 import edu.project4.rendering.MultiThreadedRenderer;
 import edu.project4.transformations.AffineTransformation;
+import edu.project4.transformations.ExponentialTransformation;
 import edu.project4.transformations.SphericalTransformation;
+import edu.project4.transformations.TangentTransformation;
+import edu.project4.transformations.Transformation;
 import edu.project4.utils.ImageFormat;
 import edu.project4.utils.ImageUtils;
-import java.awt.Desktop;
 import java.io.IOException;
 import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -21,32 +25,77 @@ public class FlameGenerationRunner {
     private FlameGenerationRunner() {
     }
 
-    @SuppressWarnings("MagicNumber") static void generateFractalFlame() throws IOException {
-        int pointsNumber = 10_000;
-        int iterationNumber = 10_000;
-        int affineNumber = 5;
-        double gamma = 0.5;
-
+    @SuppressWarnings("MagicNumber")
+    static void generateFractalFlame(
+        int pointsNumber,
+        int iterationNumber,
+        int affineNumber,
+        double gamma,
+        List<Color> colors,
+        Transformation nonLinearTransformation,
+        Path filename
+    ) throws IOException {
         MultiThreadedRenderer renderer = new MultiThreadedRenderer();
         List<AffineTransformation> affineTransformationList =
-            renderer.generateAffineTransformations(affineNumber, List.of(Color.RED, Color.GREEN));
+            renderer.generateAffineTransformations(affineNumber, colors);
 
+        LOGGER.info("Start rendering...");
         long startTime = System.nanoTime();
-        FractalImage image = renderer.render(pointsNumber,
+        FractalImage image = renderer.render(
+            pointsNumber,
             iterationNumber,
             1920,
             1080,
             affineTransformationList,
-            new SphericalTransformation()
+            nonLinearTransformation
         );
-        LOGGER.info("Rendering time: " + (System.nanoTime() - startTime) / 1e9 + " seconds");
+        long endTime = System.nanoTime();
+        LOGGER.info("Finish rendering");
+        LOGGER.info("Rendering time: " + (endTime - startTime) / 1e9 + " seconds");
 
         LogGammaCorrection correction = new LogGammaCorrection(gamma);
         correction.process(image);
 
-        Path filename = Path.of("src", "main", "resources", "project4", "single_threaded_fractal.png");
         ImageUtils.save(image, filename, ImageFormat.PNG);
+    }
 
-        Desktop.getDesktop().open(filename.toFile());
+    @SuppressWarnings("MagicNumber")
+    static void generateRandomFlames(int count) throws IOException {
+        int lowerPointsNumberEdge = 20000;
+        int upperPointsNumberEdge = 70000;
+        int lowerIterationsNumberEdge = 20000;
+        int upperIterationsNumberEdge = 70000;
+        int lowerAffineNumberEdge = 3;
+        int upperAffineNumberEdge = 15;
+        int lowerColorNumberEdge = 1;
+        int upperColorNumberEdge = 6;
+        double lowerGammaEdge = 0.3;
+        double upperGammaEdge = 1.0;
+        List<Color> availableColors =
+            List.of(Color.RED, Color.GREEN, Color.BLUE, Color.ORANGE, Color.PURPLE, Color.WHITE);
+        List<Transformation> availableNonLinearTransformations =
+            List.of(new SphericalTransformation(), new ExponentialTransformation(), new TangentTransformation());
+        Random random = new Random();
+
+        long startTime = System.nanoTime();
+        for (int i = 1; i <= count; i++) {
+            List<Color> colors = new ArrayList<>();
+            for (int j = 0; j < random.nextInt(lowerColorNumberEdge, upperColorNumberEdge); j++) {
+                colors.add(availableColors.get(random.nextInt(availableColors.size())));
+            }
+
+            generateFractalFlame(
+                random.nextInt(lowerPointsNumberEdge, upperPointsNumberEdge),
+                random.nextInt(lowerIterationsNumberEdge, upperIterationsNumberEdge),
+                random.nextInt(lowerAffineNumberEdge, upperAffineNumberEdge),
+                random.nextDouble(lowerGammaEdge, upperGammaEdge),
+                colors,
+                availableNonLinearTransformations.get(random.nextInt(availableNonLinearTransformations.size())),
+                Path.of("src", "main", "resources", "project4", "flames_batch", "fractal_%d.png".formatted(i))
+            );
+        }
+
+        long endTime = System.nanoTime();
+        LOGGER.info("Entire generation time - {} minutes", (endTime - startTime) / (1e9 * 60));
     }
 }
