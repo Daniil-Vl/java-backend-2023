0fec43a93533522b27ccd6d5d2828ffb90fca5f4
Daniil-Vl <vlasov.daniil03@gmail.com>
2023-12-10 19:28:37 +0300

    Project4: finish task 2

--- src/main/java/edu/hw9/task2/PredicateFilesFilter.java
+++ src/main/java/edu/hw9/task2/PredicateFilesFilter.java
@@ -6,49 +6,47 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ForkJoinPool;
 import java.util.concurrent.RecursiveTask;
+import java.util.function.Predicate;
 
-public class LargeDirectoriesFilter {
+public class PredicateFilesFilter {
     private final ForkJoinPool forkJoinPool = new ForkJoinPool();
 
-    public List<Path> findLargeDirs(Path initialDirectory, int minFilesNumber) {
-        RecursiveTask<List<Path>> task = new FindLargeDirTask(initialDirectory, minFilesNumber);
+    public List<Path> findFilesUsingPredicate(Path initialDirectory, Predicate<Path> predicate) {
+        RecursiveTask<List<Path>> task = new FindFileTask(initialDirectory, predicate);
         return this.forkJoinPool.invoke(task);
     }
 
-    private static class FindLargeDirTask extends RecursiveTask<List<Path>> {
-        private final int minFilesNumber;
+    private static class FindFileTask extends RecursiveTask<List<Path>> {
+
+        private final Predicate<Path> predicate;
         private final Path directory;
 
-        FindLargeDirTask(Path initialDirectory, int minFilesNumber) {
+        FindFileTask(Path initialDirectory, Predicate<Path> predicate) {
             if (!initialDirectory.toFile().isDirectory()) {
                 throw new IllegalArgumentException("Initial directory doesn't exist");
             }
             this.directory = initialDirectory;
-            this.minFilesNumber = minFilesNumber;
+            this.predicate = predicate;
         }
 
         @Override
         protected List<Path> compute() {
             List<Path> result = new ArrayList<>();
 
-            File[] innerFiles = directory.toFile().listFiles();
             List<RecursiveTask<List<Path>>> tasks = new ArrayList<>();
-            int innerFilesCount = 0;
 
+            File[] innerFiles = directory.toFile().listFiles();
             if (innerFiles != null) {
-                for (File file : innerFiles) {
-                    if (file.isDirectory()) {
-                        RecursiveTask<List<Path>> task = new FindLargeDirTask(file.toPath(), minFilesNumber);
+                for (File innerFile : innerFiles) {
+                    Path innerFilePath = innerFile.toPath();
+                    if (innerFile.isDirectory()) {
+                        var task = new FindFileTask(innerFilePath, predicate);
                         task.fork();
                         tasks.add(task);
-                    } else if (file.isFile()) {
-                        innerFilesCount++;
+                    } else if (innerFile.isFile() && predicate.test(innerFilePath)) {
+                        result.add(innerFilePath);
                     }
                 }
-            }
-
-            if (innerFilesCount > minFilesNumber) {
-                result.add(directory);
             }
 
             for (var task : tasks) {
